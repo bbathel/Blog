a0ff635269b6dc2cc819a16974cc45dd4463a2c0
bbathel <bbathel@searchinfluence.com>
2015-02-17 22:00:34 -0600

    added some tests and stuff

--- blog/test/controllers/articles_controller_test.rb
+++ blog/test/controllers/articles_controller_test.rb
@@ -2,39 +2,47 @@ require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
   setup do
-    @article = articles(:one)
+    @article = articles(:welcome_to_rails)
   end
 
   test "should get index" do
     get :index
     assert_response :success
+    assert_template 'index'
     assert_not_nil assigns(:articles)
   end
 
   test "should get new" do
+    login_as(:eugene)
     get :new
     assert_response :success
   end
 
   test "should create article" do
+    login_as(:eugene)
     assert_difference('Article.count') do
       post :create, article: { body: @article.body, excerpt: @article.excerpt, location: @article.location, published_at: @article.published_at, title: @article.title }
     end
-
+    assert_response :redirect
     assert_redirected_to article_path(assigns(:article))
   end
 
   test "should show article" do
-    get :show, id: @article
+    get :show, id: @article.to_param
     assert_response :success
+    assert_template 'show'
+    assert_not_nill assigns(:article)
+    assert assigns(:article).valid?
   end
 
   test "should get edit" do
+    login_as(:eugene)
     get :edit, id: @article
     assert_response :success
   end
 
   test "should update article" do
+    login_as(:eugene)
     patch :update, id: @article, article: { body: @article.body, excerpt: @article.excerpt, location: @article.location, published_at: @article.published_at, title: @article.title }
     assert_redirected_to article_path(assigns(:article))
   end
